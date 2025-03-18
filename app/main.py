987cca7373e263ff8fcfb08b89525cbcbe131e76
Arda-Yaz <ardayaz.buss@gmail.com>
2025-03-18 22:14:34 +0300

    commit

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pickle
 import pandas as pd
+import plotly.graph_objects as go
 
 
 def get_clean_data():
@@ -60,6 +61,35 @@ def add_sidebar():
         )
     return input_dict
 
+def get_radar_chart(input_data):
+    categories = ['processing cost', 'mechanical properties', 'chemical stability',
+                  'thermal stability', 'device integration']
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatterpolar(
+        r=[1, 5, 2, 2, 3],
+        theta=categories,
+        fill='toself',
+        name='Product A'
+    ))
+    fig.add_trace(go.Scatterpolar(
+        r=[4, 3, 2.5, 1, 2],
+        theta=categories,
+        fill='toself',
+        name='Product B'
+    ))
+
+    fig.update_layout(
+        polar=dict(
+            radialaxis=dict(
+                visible=True,
+                range=[0, 5]
+            )),
+        showlegend=False
+    )
+
+    return fig
 
 def main():
     st.set_page_config(
@@ -82,7 +112,8 @@ def main():
     col1, col2 = st.columns([4,1])
 
     with col1:
-        st.write("this is column 1")
+        radar_chart = get_radar_chart(input_data)
+        st.plotly_chart(radar_chart)
     with col2:
         st.write("this is column 2")
 
